bac144f5edf584dfb1eade027d66f0c6dadf0f40
kusui-user <kusui@foodnetwork.co.jp>
2021-08-06 06:07:41 +0900

    更新

--- nuxt-foodnet-animation/pages/index.vue
+++ nuxt-foodnet-animation/pages/index.vue
@@ -1,49 +1,166 @@
 <template>
-  <div>
-    <section>
-   
-      <h2 id="work-trigger" class="section-title effect-fade-side">Works</h2>
-      <div class="article-body works">
-        <div class="article-content">
-          <p>技術ブログ</p>
-          <a 
-            id="work-image" href="https://mmtomitomimm.blogspot.com/"
-            target="_blank" >
-            <!-- <img src="~/assets/images/blog.png" /> -->
-          </a>
-        </div>
-      </div>
-    </section>
+  <div class="page-index">
+    <div class="title-opening">
+      <p id="title">mogumogu...</p>
+    </div>
+    <div class="effect-delay-display">
+      <section>
+     <p>Finished eating!</p>
+      </section>
+    </div>
   </div>
 </template>
 <script>
+import { TweenMax, TimelineMax } from 'gsap'
 export default {
   mounted() {
-    const scene1 = this.$scrollmagic
-      .scene({
-        triggerElement: "#work-trigger",
-        triggerHook: 0.5,
-        duration: 300,
-        reverse: false,
-      })
-      .setTween("#work-image", {
+    const timeline = new TimelineMax()
+    timeline
+      .add([
+        TweenMax.to('#title', 0.1, {
+          css: {
+            position: 'absolute',
+            opacity: '0',
+            width: '100px',
+            right: '40%',
+            height: '50px',
+            overflow: 'hidden'
+          }
+        })
+      ])
+      .to('#title', 1, {
         css: {
-          opacity: "1",
-        },
-      });
-    // ---- ↓ triggerのヘルプを表示 ----
-    // .addIndicators({ name: 'OK' })
+          width: '120px',
+          opacity: '0.5'
+        }
+      })
+      .to('#title', 2, {
+        css: {
+          width: '300px',
+          opacity: '1'
+        }
+      })
+      .to('#title', 2.5, {
+        css: {
+          opacity: '0',
+          scale: '1.5'
+        }
+      })
 
-    this.$scrollmagic.addScene(scene1);
-  },
-};
+  }
+}
 </script>
-<style  scoped  lang="scss">
 
+<style scoped lang="scss">
+body {
+  background: #fff;
+  padding: 20px;
+  font-family: Helvetica;
+}
 
-// ---- ↓ アニメーション実行前のスタイル（非表示にしておく） ----
- #work-image {
-   opacity: 0;
- }
+.content {
+  position: absolute;
+  top: 50vh;
+  right: 30vw;
+  width: 200px;
+}
+
+.msg {
+  display: inline-block;
+}
+
+.title-opening {
+  position: absolute;
+  top: 50vh;
+  margin: auto;
+  text-align: center;
+  width: 85vw;
+  font-size: 30px;
+  word-break: break-all;
+}
+
+.effect-delay-display {
+  animation-name: disp-delay;
+  animation-delay: 5s;
+  animation-duration: 1s;
+  animation-iteration-count: 1;
+  animation-fill-mode: backwards;
+}
+
+@keyframes disp-delay {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+// .tougue-solid.icon {
+//   color: #000;
+//   position: absolute;
+//   margin-left: 2px;
+//   margin-top: 2px;
+//   width: 16px;
+//   height: 16px;
+//   border-radius: 50%;
+//   border: solid 1px currentColor;
+//   background-color: currentColor;
+//   display: inline-block;
+//   margin: 16px;
+// }
+
+// .tougue-solid.icon:before {
+//   content: '';
+//   position: absolute;
+//   left: 4px;
+//   top: 4px;
+//   width: 2px;
+//   height: 2px;
+//   border-radius: 50%;
+//   color: white;
+//   background-color: currentColor;
+// }
+
+// .tougue-solid.icon:after {
+//   content: '';
+//   position: absolute;
+//   right: 4px;
+//   top: 4px;
+//   width: 2px;
+//   height: 2px;
+//   border-radius: 50%;
+//   color: white;
+//   background-color: currentColor;
+// }
+
+// .tougue-solid.icon i {
+//   position: absolute;
+//   left: 4px;
+//   top: 10px;
+//   width: 8px;
+//   height: 1px;
+//   color: white;
+//   background-color: currentColor;
+//   -webkit-transform: rotate(-3deg);
+//           transform: rotate(-3deg);
+// }
+
+// .tougue-solid.icon i:before {
+//   content: '';
+//   position: absolute;
+//   top: -1px;
+//   right: 0px;
+//   width: 2px;
+//   height: 2px;
+//   border-radius: 2px;
+//   color: white;
+//   border-top: solid 1px transparent;
+//   border-right: solid 1px transparent;
+//   border-bottom: solid 1px currentColor;
+//   border-left: solid 1px currentColor;
+//   -webkit-transform: rotate(-45deg);
+//           transform: rotate(-45deg);
+// }
+
 
 </style>
