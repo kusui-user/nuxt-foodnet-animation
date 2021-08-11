0f70ee9e06fff368e5d4c277e92e580f484a19e6
kusui.takashi <kusui@foodnetwork.co.jp>
2021-08-11 16:50:11 +0900

    更新

--- nuxt-foodnet-animation/nuxt.config.js
+++ nuxt-foodnet-animation/nuxt.config.js
@@ -43,5 +43,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
+
+ 
 }
--- nuxt-foodnet-animation/pages/index.vue
+++ nuxt-foodnet-animation/pages/index.vue
@@ -1,55 +1,61 @@
 <template>
- <div>
-  <div class="title-opening">
-    <p id="title">mogumogu....</p>
+  <div>
+    <div id="title">ABCD</div>
   </div>
-  <div class="content effect-delay-display">
-    <div class="msg">
-      <p>Finished eating!</p>
-    </div>
-    <div class="tougue-solid icon"><i></i></div>
-  </div>
-</div>
 </template>
 <script>
-import { TweenMax, TimelineMax } from 'gsap'
+// import { TweenMax, TimelineMax } from "gsap";
 export default {
+  middleware: 'midle',
   mounted() {
     const timeline = new TimelineMax()
     timeline
       .add([
-        TweenMax.to('#title', 0.1, {
+        TweenMax.to('#title', 1, {
           css: {
-            position: 'absolute',
-            opacity: '0',
-            width: '100px',
-            right: '40%',
-            height: '50px',
-            overflow: 'hidden'
+            background: '#F3FFD8',
+            transition: 'ease',
           }
         })
       ])
-      .to('#title', 1, {
+      .to('#title', 1.1, {
         css: {
-          width: '120px',
-          opacity: '0.5'
+          background: '#E9FFA5',
         }
       })
-      .to('#title', 2, {
+      .to('#title', 1.2, {
         css: {
-          width: '300px',
-          opacity: '1'
+          background: '#DBFF71',
         }
       })
-      .to('#title', 2.5, {
+      .to('#title', 1.3, {
         css: {
-          opacity: '0',
-          scale: '1.5'
+         background: '#D0FF43',
+        }
+      })
+      .to('#title', 1.4, {
+        css: {
+         background: '#D7EEFF',
+        }
+      })
+      .to('#title', 1.5, {
+        css: {
+         background: '#A7F1FF',
+        }
+      })
+      .to('#title', 1.6, {
+        css: {
+         background: '#60EEFF',
+        }
+      })
+      .to('#title', 1.7, {
+        css: {
+         background: '#00ECFF',
         }
       })
 
   }
-}
+};
 </script>
 
 <style scoped lang="scss">
@@ -59,113 +65,11 @@ body {
   font-family: Helvetica;
 }
 
-.page-index{
-  position: relative;
-}
-
-.content {
-  position: absolute;
-  top: 50vh;
-  right: 30vw;
-  width: 200px;
-}
-
-.msg {
-  display: inline-block;
-}
-
-.title-opening {
-  position: absolute;
-  top: 50vh;
-  margin: auto;
+#title {
+  // width: 500px;
+  height: 600px;
+  background-color: red;
   text-align: center;
-  width: 85vw;
-  font-size: 30px;
-  word-break: break-all;
+  line-height: 600px;
 }
-
-.effect-delay-display {
-  animation-name: disp-delay;
-  animation-delay: 5s;
-  animation-duration: 1s;
-  animation-iteration-count: 1;
-  animation-fill-mode: backwards;
-}
-
-@keyframes disp-delay {
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-}
-.tougue-solid.icon {
-  color: #000;
-  position: absolute;
-  margin-left: 2px;
-  margin-top: 2px;
-  width: 16px;
-  height: 16px;
-  border-radius: 50%;
-  border: solid 1px currentColor;
-  background-color: currentColor;
-  display: inline-block;
-  margin: 16px;
-}
-
-.tougue-solid.icon:before {
-  content: '';
-  position: absolute;
-  left: 4px;
-  top: 4px;
-  width: 2px;
-  height: 2px;
-  border-radius: 50%;
-  color: white;
-  background-color: currentColor;
-}
-
-.tougue-solid.icon:after {
-  content: '';
-  position: absolute;
-  right: 4px;
-  top: 4px;
-  width: 2px;
-  height: 2px;
-  border-radius: 50%;
-  color: white;
-  background-color: currentColor;
-}
-
-.tougue-solid.icon i {
-  position: absolute;
-  left: 4px;
-  top: 10px;
-  width: 8px;
-  height: 1px;
-  color: white;
-  background-color: currentColor;
-  -webkit-transform: rotate(-3deg);
-          transform: rotate(-3deg);
-}
-
-.tougue-solid.icon i:before {
-  content: '';
-  position: absolute;
-  top: -1px;
-  right: 0px;
-  width: 2px;
-  height: 2px;
-  border-radius: 2px;
-  color: white;
-  border-top: solid 1px transparent;
-  border-right: solid 1px transparent;
-  border-bottom: solid 1px currentColor;
-  border-left: solid 1px currentColor;
-  -webkit-transform: rotate(-45deg);
-          transform: rotate(-45deg);
-}
-
-
 </style>
