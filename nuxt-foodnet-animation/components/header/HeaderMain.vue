324e507379cffab99b5013aa5976738ddde900a5
kusui-user <kusui@foodnetwork.co.jp>
2021-08-18 06:14:13 +0900

    更新

---
+++ nuxt-foodnet-animation/components/header/HeaderMain.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="header-main">
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header-main{
+  width: 100%;
+  height: 100vh;
+  background-color: skyblue;
+}
+</style>
