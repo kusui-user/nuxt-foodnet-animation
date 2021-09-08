53204dcee2c1399940d431e9472dee8a668a05d9
kusui.takashi <kusui@foodnetwork.co.jp>
2021-09-08 15:07:04 +0900

    更新

--- nuxt-foodnet-animation/nuxt.config.js
+++ nuxt-foodnet-animation/nuxt.config.js
@@ -2,7 +2,7 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
-  Target: static,
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
